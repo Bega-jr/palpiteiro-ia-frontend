764eb24281a71a56a2a68fec4eb6db9bc91ced38
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-19 17:19:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
-/* see below: src/App.js full content */
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Estatisticas from './pages/Estatisticas';
+import Historico from './pages/Historico';
+import GerarAvancado from './pages/GerarAvancado';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/estatisticas" element={<Estatisticas />} />
+        <Route path="/historico" element={<Historico />} />
+        <Route path="/gerar-avancado" element={<GerarAvancado />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
