bb0cee874e3d6e3045dc3e2526be464319b205b3
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-19 09:06:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App';  // ou './App.jsx'
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
