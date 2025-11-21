793d70ba9b0ce006941b501d5ec0d02429513ddb
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-21 09:55:56 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+import "./styles/global.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIAcBJg1FLwMBAKhAcKePw-bxi1piVlXw",
+  authDomain: "palpiteiro-ia.firebaseapp.com",
+  projectId: "palpiteiro-ia",
+  storageBucket: "palpiteiro-ia.firebasestorage.app",
+  messagingSenderId: "1063099774350",
+  appId: "1:1063099774350:web:fa22f335a19e885e95a0df",
+  measurementId: "G-RP2ZZKMLPF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
