66f0de5fad1f8da206b481aa7154ccdc5ed41b0b
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-11-24 13:23:04 -0300

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import "../styles/loading.css";
 
 export default function Loading() {
   return (
-    <div className="loading-container">
+    <div className="loading-spinner">
       <div className="spinner"></div>
       <p>Carregando...</p>
     </div>
